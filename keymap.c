5783959856b7dc004a18112c107e5412022d0d21
Shihpin Tseng <deftsp@gmail.com>
2017-05-04 10:50:28 +0800

    minor change.

--- keymap.c
+++ keymap.c
@@ -217,9 +217,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------.           ,-----------------------------------------------------.
  * |           |      |      |      |      |      |      |           |      |      |      |      |      |      |           |
  * |-----------+------+------+------+------+-------------|           |------+------+------+------+------+------+-----------|
- * |           |      |      |      |      |      |      |           |      |      |      |      |      |      |           |
+ * |           |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |    F11    |
  * |-----------+------+------+------+------+------|      |           |      |------+------+------+------+------+-----------|
- * |           |      |      |      |      |      |------|           |------|      |      |      |      |      |           |
+ * |           |   1  |   2  |   3  |   4  |  5   |------|           |------|   6  |  7   |   8  |   9  |   0  |    F12    |
  * |-----------+------+------+------+------+------|      |           |      |------+------+------+------+------+-----------|
  * |           |      |      |      |      |      |      |           |      |      |      |      |      |      |           |
  * `-----------+------+------+------+------+-------------'           `-------------+------+------+------+------+-----------'
@@ -238,7 +238,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,      KC_NO,       KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,
         KC_NO,      KC_F1,       KC_F2,      KC_F3,   KC_F4,   KC_F5,   KC_TRNS,
         KC_NO,      KC_1,        KC_2,       KC_3,    KC_4,    KC_5,
-        KC_NO,      KC_NO,       KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
+        KC_TRNS,    KC_NO,       KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
         KC_NO,      KC_NO,       KC_NO,      KC_NO,   KC_NO,
 
         KC_VOLD, KC_VOLU,
@@ -249,7 +249,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,
         KC_TRNS,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,
                    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_F12,
-        KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,
+        KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_TRNS,
         KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,
 
         KC_TRNS,   KC_NO,
