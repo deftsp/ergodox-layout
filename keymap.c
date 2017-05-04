8cfac2279a5571e9e936a7c3daedc84c7a826dc9
Shihpin Tseng <deftsp@gmail.com>
2017-05-04 13:29:42 +0800

    minor change

--- keymap.c
+++ keymap.c
@@ -246,7 +246,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_NO,   KC_TRNS,
 
         // right hand
-        KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,
+        KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,
         KC_TRNS,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,
                    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_F12,
         KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_TRNS,
