a0e47788fdb0a857f629c53c0a8ccbb8376fd825
Shihpin Tseng <deftsp@gmail.com>
2017-05-04 10:58:59 +0800

    minor change.

--- keymap.c
+++ keymap.c
@@ -124,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Base Layer
  *
  * ,-----------------------------------------------------.           ,-----------------------------------------------------.
- * |     `     | 1  ! | 2  @ | 3  # | 4  $ | 5  % | ESC  |           |  Fx  | 6  ^ | 7  & | 8  & | 9  ( | 0  ) |  Backsp   |
+ * |     `     | 1  ! | 2  @ | 3  # | 4  $ | 5  % | 6  ^ |           |  Fx  | 6  ^ | 7  & | 8  & | 9  ( | 0  ) |  Backsp   |
  * |-----------+------+------+------+------+-------------|           |------+------+------+------+------+------+-----------|
  * |  KC_TAB   |   Q  |   W  |   E  |   R  |   T  |   (  |           |  )   |   Y  |   U  |   I  |   O  |  P   |     \     |
  * |-----------+------+------+------+------+------|   [  |           |  ]   |------+------+------+------+------+-----------|
@@ -144,7 +144,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [BASE] = KEYMAP(
         // left hand
-        TD(CT_GRTI),         KC_1,          KC_2,          KC_3,         KC_4,         KC_5,                KC_ESC,
+        TD(CT_GRTI),         KC_1,          KC_2,          KC_3,         KC_4,         KC_5,                KC_6,
         KC_TAB,              KC_Q,          KC_W,          KC_E,         KC_R,         KC_T,                TD(CT_LBP),
         TD(CT_EA),           KC_A,          KC_S,          KC_D,         KC_F,         KC_G,
         KC_LSFT,             LCTL_T(KC_Z),  KC_X,          KC_C,         KC_V,         KC_B,                LT(PROG, KC_MINUS),
