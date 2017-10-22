3e1bef4f6d0df70688e0a5190272c687c12b7eb0
Shihpin Tseng <deftsp@gmail.com>
2017-10-23 01:11:45 +0800

    ESC cancel oneshot and oneshot lock.

--- keymap.c
+++ keymap.c
@@ -121,7 +121,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------+------+------+------+------+------|  -   |           |  =   |------+------+------+------+------+-----------|
  * |   PgDn    |   Z  |   X  |   C  |   V  |   B  | LT->1|           | LT->1|   N  |   M  |   ,  |   .  |  /   |     -     |
  * `-----------+------+------+------+------+-------------'           `-------------+------+------+------+------+-----------'
- *     |       |      |      | LCTL |Backsp|                                       | Space| RCTL |      |      |       |
+ *     |       |      |      | LCTL |Backsp|                                       | Space| RCTL |      | REKBD|       |
  *     `-----------------------------------'                                       `-----------------------------------'
  *                                         ,-------------.           ,-------------.
  *                                         | LAlt | GUI  |           | MDIA | Del  |
@@ -133,7 +133,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [BASE] = KEYMAP(
         // left hand
-        KC_MPLY,             KC_1,          KC_2,          KC_3,         KC_4,         KC_5,                M(REKBD),
+        KC_MPLY,             KC_1,          KC_2,          KC_3,         KC_4,         KC_5,                KC_NO,
         KC_GRV,              KC_Q,          KC_W,          KC_E,         KC_R,         KC_T,                KC_TAB,
         KC_PGUP,             KC_A,          KC_S,          KC_D,         KC_F,         KC_G,
         KC_PGDOWN,           KC_Z,          KC_X,          KC_C,         KC_V,         KC_B,                LT(PROG, KC_MINS),
@@ -148,7 +148,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ENTER,            KC_Y,          KC_U,         KC_I,          KC_O,         KC_P,                KC_EQL,
                              KC_H,          KC_J,         KC_K,          KC_L,         KC_SCLN,             KC_QUOT,
         LT(PROG, KC_EQL),    KC_N,          KC_M,         KC_COMM,       KC_DOT,       KC_SLSH,             KC_MINS,
-        KC_SPC,              F(F_RCTL),     KC_NO,        KC_NO,         TD(CT_SR),
+        KC_SPC,              F(F_RCTL),     KC_NO,        M(REKBD),      TD(CT_SR),
 
         OSL(PROG), KC_DEL,
         KC_LEAD,
@@ -693,6 +693,13 @@ bool process_record_user (uint16_t keycode, keyrecord_t *record) {
                 clear_oneshot_mods ();
                 queue = false;
             }
+
+            if (get_mods() && get_oneshot_locked_mods()) {
+                clear_oneshot_locked_mods();
+                clear_mods();
+                queue = false;
+            }
+
             return queue;
           }
       break;
