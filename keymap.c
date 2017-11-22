e9e6043825bd626e68278b864c640849a84eb217
Shihpin Tseng <deftsp@gmail.com>
2017-11-22 11:56:08 +0800

    minor change

--- keymap.c
+++ keymap.c
@@ -134,14 +134,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------+------+------+------+------+------|  -   |           |  =   |------+------+------+------+------+-----------|
  * |   PgDn    |   Z  |   X  |   C  |   V  |   B  | LT->1|           | LT->1|   N  |   M  |   ,  |   .  |  /   |     -     |
  * `-----------+------+------+------+------+-------------'           `-------------+------+------+------+------+-----------'
- *     |       |      |      | LCTL |Backsp|                                       | Space| RCTL |      | REKBD|       |
+ *     |       |      |      | LCTL | LCTL |                                       | RCTL | RCTL |      | REKBD|       |
  *     `-----------------------------------'                                       `-----------------------------------'
  *                                         ,-------------.           ,-------------.
  *                                         | LAlt | GUI  |           | MDIA | Del  |
  *                                  ,------|------|------|           |------+------+------.
  *                                  |      |      | Ctrl |           | LEAD |      |      |
- *                                  | LGUI |LShift|------|           |------|RShift| RGUI |
- *                                  |      |      | ESC  |           | Del  |      |      |
+ *                                  |Backsp| LGUI |------|           |------| RALT | Space|
+ *                                  |      |      |LShift|           |RShift|      |      |
  *                                  `--------------------'           `--------------------'
  */
     [BASE] = KEYMAP(
@@ -150,22 +150,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,              KC_Q,          KC_W,          KC_E,         KC_R,         KC_T,                KC_TAB,
         KC_PGUP,             KC_A,          KC_S,          KC_D,         KC_F,         KC_G,
         KC_PGDOWN,           KC_Z,          KC_X,          KC_C,         KC_V,         KC_B,                LT(PROG, KC_MINS),
-        KC_NO,               KC_NO,         KC_NO,         F(F_LCTL),    KC_BSPC,
+        KC_NO,               KC_NO,         KC_NO,         F(F_LCTL),    F(F_LCTL),
 
         F(F_LALT), F(F_LGUI),
         F(F_LCTL),
-        F(F_LGUI),  F(F_LSFT), TT(PROG),
+        KC_BSPC,   F(F_LGUI),  F(F_LSFT),
 
         // right hand
         M(Fx),               KC_6,          KC_7,         KC_8,          KC_9,         KC_0,                KC_BSPC,
         KC_ENTER,            KC_Y,          KC_U,         KC_I,          KC_O,         KC_P,                KC_EQL,
                              KC_H,          KC_J,         KC_K,          KC_L,         KC_SCLN,             KC_QUOT,
         LT(PROG, KC_EQL),    KC_N,          KC_M,         KC_COMM,       KC_DOT,       KC_SLSH,             KC_MINS,
-        KC_SPC,              F(F_RCTL),     KC_NO,        M(REKBD),      TD(CT_SR),
+        F(F_RCTL),           F(F_RCTL),     KC_NO,        M(REKBD),      TD(CT_SR),
 
         OSL(PROG), KC_DEL,
         KC_LEAD,
-        TT(PROG), F(F_RSFT), F(F_LALT)
+        F(F_RSFT), F(F_LALT), KC_SPC
         ),
 /* Keymap 1: Prog Layer
  *
